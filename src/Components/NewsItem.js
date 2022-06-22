fb959bac03f7f080f66c456d9a27b703fa3ad435
VanshArora08 <kumar.vansharora@gmail.com>
2022-06-23 00:19:46 +0530

    Made card structure for NewsItems and defined one row with three coloums in a row for news items

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title,description,imageUrl,newsUrl}=this.props;
     return (
       <div>
-        This is a news Item
+        <div className="card grid-md-4" style={{width: "18rem"}}>
+        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
       </div>
     )
   }
