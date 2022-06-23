9966a5f19fd89444146b0f8ebc9737d7f5769b01
VanshArora08 <kumar.vansharora@gmail.com>
2022-06-24 01:53:05 +0530

    changed navbar theme to dark

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
         <div className="container-fluid">
             <a className="navbar-brand" href="/"><span>Daily Bugle</span></a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
